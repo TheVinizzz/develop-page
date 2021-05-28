1690a7a24f9bb76d818b9a6b2768a4ba3c2ed76f
Vinicius Borges <viniprograming@gmail.com>
2021-05-28 17:52:15 -0300

    New teste tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 export default function Index() {
 	return (
-		<div className="p-4 shadow rounded bg-white">
-			<h1 className="text-purple-500 leading-normal">Next.js</h1>
-			<p className="text-gray-500">with Tailwind CSS</p>
+		<div>
+			<div className="text-2xl">
+				<h1 className="text-gray-500">Testando</h1>
+			</div>
 		</div>
 	)
 }
